64e4dd835ac5d1da31d11cc01de76c1dfeaf68fa
Raymond.K <kraymond37@protonmail.com>
2021-08-22 11:16:30 +0800

    add sign method

--- data/signing.go
+++ data/signing.go
@@ -1,6 +1,9 @@
 package data
 
-import "github.com/rubblelabs/ripple/crypto"
+import (
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/rubblelabs/ripple/crypto"
+)
 
 func Sign(s Signer, key crypto.Key, sequence *uint32) error {
 	s.InitialiseForSigning()
@@ -10,6 +13,28 @@ func Sign(s Signer, key crypto.Key, sequence *uint32) error {
 		return err
 	}
 	sig, err := crypto.Sign(key.Private(sequence), hash.Bytes(), append(s.SigningPrefix().Bytes(), msg...))
+	if err != nil {
+		return err
+	}
+	*s.GetSignature() = VariableLength(sig)
+	hash, _, err = Raw(s)
+	if err != nil {
+		return err
+	}
+	copy(s.GetHash().Bytes(), hash.Bytes())
+	return nil
+}
+
+func SignWithPrivKey(s Signer, key []byte) error {
+	s.InitialiseForSigning()
+	privKey, pubKey := btcec.PrivKeyFromBytes(btcec.S256(), key)
+	copy(s.GetPublicKey().Bytes(), pubKey.SerializeCompressed())
+
+	hash, msg, err := SigningHash(s)
+	if err != nil {
+		return err
+	}
+	sig, err := crypto.Sign(privKey.Serialize(), hash.Bytes(), append(s.SigningPrefix().Bytes(), msg...))
 	if err != nil {
 		return err
 	}
